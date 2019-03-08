11ad628baa099c43a63e67548724d8e89dc81af0
GrigoryCode <grigorycode@gmail.com>
2019-03-08 23:10:40 +0300

    Add categories and brands

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from ecommerce.models import Category, Brand
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
